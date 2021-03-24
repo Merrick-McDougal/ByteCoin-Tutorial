3f5a7bc5f24efebc9da903eb3b56a9dd1b6f68d0
Merrick McDougal <Merrick@mrmcdougal.com>
2021-03-24 02:02:54 -0500

    Final Product

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,16 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Merrick Mcdougal on 3/17/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
+
+
+
